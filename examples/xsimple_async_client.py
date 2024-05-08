d1277c76e36dd825f5fe896e9706e2dbc5cd3921
Maksim Novozhilov <ufamax@gmail.com>
2024-05-09 00:22:32 +0100

    added xsimple_async_client.py

---
+++ examples/xsimple_async_client.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+"""Pymodbus asynchronous client example.
+
+An example of a single threaded synchronous client.
+
+usage: simple_client_async.py
+
+All options must be adapted in the code
+The corresponding server must be started before e.g. as:
+    python3 server_sync.py
+"""
+import asyncio
+
+import pymodbus.client as ModbusClient
+from pymodbus import (
+    ExceptionResponse,
+    FramerType,
+    ModbusException,
+    pymodbus_apply_logging_config,
+)
+
+
+async def run_async_simple_client(comm, host, port, framer=FramerType.SOCKET):
+    """Run async client."""
+    # activate debugging
+    pymodbus_apply_logging_config("DEBUG")
+
+    print("get client")
+    if comm == "tcp":
+        client = ModbusClient.AsyncModbusTcpClient(
+            host,
+            port=port,
+            framer=framer,
+            # timeout=10,
+            # retries=3,
+            # retry_on_empty=False,
+            # source_address=("localhost", 0),
+        )
+    elif comm == "udp":
+        client = ModbusClient.AsyncModbusUdpClient(
+            host,
+            port=port,
+            framer=framer,
+            # timeout=10,
+            # retries=3,
+            # retry_on_empty=False,
+            # source_address=None,
+        )
+    elif comm == "serial":
+        client = ModbusClient.AsyncModbusSerialClient(
+            port,
+            framer=framer,
+            # timeout=10,
+            # retries=3,
+            # retry_on_empty=False,
+            # strict=True,
+            baudrate=9600,
+            bytesize=8,
+            parity="N",
+            stopbits=1,
+            # handle_local_echo=False,
+        )
+    elif comm == "tls":
+        client = ModbusClient.AsyncModbusTlsClient(
+            host,
+            port=port,
+            framer=FramerType.TLS,
+            # timeout=10,
+            # retries=3,
+            # retry_on_empty=False,
+            # sslctx=sslctx,
+            certfile="../examples/certificates/pymodbus.crt",
+            keyfile="../examples/certificates/pymodbus.key",
+            # password="none",
+            server_hostname="localhost",
+        )
+    else:
+        print(f"Unknown client {comm} selected")
+        return
+
+    print("connect to server")
+    await client.connect()
+    # test client is connected
+    assert client.connected
+
+    print("get and verify data")
+    try:
+        # See all calls in client_calls.py
+        # rr = await client.read_coils(1, 1, slave=1)
+        # client.ctx.recv_buffer = b"" # clear receved buffer
+
+        # rr = await client.read_input_registers(11, 1, slave=5)
+        # rr = await client.read_input_registers(5, 12, slave=5)
+        # rr = await client.read_input_registers(11, 1, slave=5)
+
+        # rr = await client.write_register(257, 15000, slave=5)
+        rr = await client.read_holding_registers(257, 1, slave=5)
+
+        if rr:
+            print('registers:', rr.registers)
+        else:
+            print('unable to read registers')
+
+    except ModbusException as exc:
+        print(f"EXCEPTION: {exc}")
+        client.close()
+        return
+    if rr.isError():
+        print(f"Received Modbus library error({rr})")
+        client.close()
+        return
+    if isinstance(rr, ExceptionResponse):
+        print(f"Received Modbus library exception ({rr})")
+        # THIS IS NOT A PYTHON EXCEPTION, but a valid modbus message
+        client.close()
+
+    print("close connection")
+    client.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(
+        run_async_simple_client("tcp", "192.168.5.198", 502), debug=True
+    )
